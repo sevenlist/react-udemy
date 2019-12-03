450d110a1b4710e7f48d69769edfda65085a7c32
sevenlist <sevenlist@web.de>
2019-12-03 13:19:28 +0100

    Sort import statements alphabetically

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuild';
 import Layout from './components/Layout/Layout';
+import React from 'react';
 
 function App() {
   return (
