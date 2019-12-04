1d4d7f9929b517cc6e5ead931e5a41acb7e54c63
sevenlist <sevenlist@web.de>
2019-12-04 17:14:48 +0100

    Capitalize constant letters

--- burger-builder/src/components/BuildControls/BuildControls.js
+++ burger-builder/src/components/BuildControls/BuildControls.js
@@ -2,11 +2,11 @@ import BuildControl from "./BuildControl/BuildControl";
 import React from 'react';
 import styles from './BuildControls.module.css';
 
-const labels = ['Salad', 'Bacon', 'Cheese', 'Meat'];
+const LABELS = ['Salad', 'Bacon', 'Cheese', 'Meat'];
 
 const BuildControls = () =>
     <div className={styles.buildControls}>
-        {labels.map(label => <BuildControl key={label.toLowerCase()} label={label} />)}
+        {LABELS.map(label => <BuildControl key={label.toLowerCase()} label={label} />)}
     </div>;
 
 export default BuildControls;
