a0527af76229d075cae41414c54ab9f7f0a49fa2
sevenlist <sevenlist@web.de>
2019-12-10 15:27:13 +0100

    Reduce height for burger in checkout summary

--- burger-builder/src/components/order/CheckoutSummary/CheckoutSummary.js
+++ burger-builder/src/components/order/CheckoutSummary/CheckoutSummary.js
@@ -7,7 +7,7 @@ import styles from './CheckoutSummary.module.css';
 const CheckoutSummary = props =>
     <div className={styles.checkoutSummary}>
         <h1>We hope it tastes well!</h1>
-        <div style={{width: "100%", height: "300px", margin: "auto"}}>
+        <div style={{width: "100%", margin: "auto"}}>
             <Burger ingredients={props.ingredients} />
         </div>
         <Button onClick={props.onCancelCheckout} type='cancel'>CANCEL</Button>
