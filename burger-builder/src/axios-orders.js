fa3e65e99af668fbc6e01a560fa71b9898c874e6
sevenlist <sevenlist@web.de>
2019-12-11 12:09:53 +0100

    Continue development of "burger-builder" in its own repo
    https://github.com/sevenlist/burger-builder

--- burger-builder/src/axios-orders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://my-delicious-burger.firebaseio.com/'
-});
-
-export default instance;
--- burger-builder/src/components/ui/Spinner/Spinner.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './Spinner.module.css';
-
-const Spinner = () => <div className={styles.loader}>Loading...</div>;
-
-export default Spinner;
